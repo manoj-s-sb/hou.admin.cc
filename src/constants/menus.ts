7a7a443b96a31db5660d4a50e1fb930f0d208c47
manoj-s-sb <manoj@stancebeam.com>
2025-12-24 15:35:46 +0530

    new coach schuling

--- src/constants/menus.ts
+++ src/constants/menus.ts
@@ -1,9 +1,9 @@
 const menus = [
-  {
-    path: '/dashboard',
-    label: 'Dashboard',
-    icon: '/assets/dashboard.svg',
-  },
+  // {
+  //   path: '/dashboard',
+  //   label: 'Dashboard',
+  //   icon: '/assets/dashboard.svg',
+  // },
   // {
   //   path: '/users',
   //   label: 'Users',
@@ -29,11 +29,11 @@ const menus = [
     label: 'Slot Bookings',
     icon: '/assets/slot-bookings.svg',
   },
-  {
-    path: '/coach-schedule',
-    label: 'Coach Schedule',
-    icon: '/assets/coach-schedule.svg',
-  },
+  // {
+  //   path: '/coach-schedule',
+  //   label: 'Coach Schedule',
+  //   icon: '/assets/coach-schedule.svg',
+  // },
 ];
 
 export default menus;
